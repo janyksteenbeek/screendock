e4c1bc3cf89a228471780f530615bc4c7e6ed90f
Janyk <me@janyk.dev>
2024-09-05 21:23:57 +0200

    Initial Commit

---
+++ ScreenDock/ScreenDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenDockApp.swift
+//  ScreenDock
+//
+//  Created by Janyk Steenbeek on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreenDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
